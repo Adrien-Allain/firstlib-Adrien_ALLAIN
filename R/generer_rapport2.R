1344d22e78ee25ba916ebd2e8a2e0b792c5b46a9
Adrien ALLAIN <adrienallain@MacBook-Air-de-Adrien.local>
2025-02-28 20:57:43 +0100

    fonction generer rapport

---
+++ R/generer_rapport2.R
@@ -0,0 +1,12 @@
+generer_rapport2 <- function(commune, departement, output) {
+  quarto::quarto_render(
+    input = file.path("Users","adrienallain","Desktop","Master","S2","R avancé","Projet","firstlibAdrienALLAIN",
+                      "inst", "modele.qmd"),
+
+    output_file = output,
+    execute_params = list(
+      code_commune = commune,
+      code_departement = departement
+    )
+  )
+}
