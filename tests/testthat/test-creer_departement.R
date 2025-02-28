fbd094efdc3b677af01f3a7faedbd7723731c975
Adrien ALLAIN <adrienallain@MacBook-Air-de-Adrien.local>
2025-02-28 15:30:25 +0100

    test modif

--- tests/testthat/test-creer_departement.R
+++ tests/testthat/test-creer_departement.R
@@ -6,12 +6,12 @@ test_that("La classe 'departement' est ajoutée si elle n'est pas présente", {
     Libellé.du.département = c("Loire-Atlantique", "Yvelines", "Bouches-du-Rhône", "Côtes-d'Armor")
   )
 
-  # Vérifier que la classe 'commune' n'est pas présente avant
+  # Vérifier que la classe 'departement' n'est pas présente avant
   expect_false("departement" %in% class(df_test))
 
-  # Appeler la fonction creer_commune
+  # Appeler la fonction creer_departement
   df_test <- creer_departement(df_test)
 
-  # Vérifier que la classe 'commune' a bien été ajoutée
+  # Vérifier que la classe 'departement' a bien été ajoutée
   expect_true("departement" %in% class(df_test))
 })
